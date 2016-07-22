5583a6aa6c141b861473dec16b4129243f234ed8
javenkn <javenkn7@yahoo.com>
2016-07-21 19:35:37 -1000

    completed the third task for queries across multiple tables

--- joins.sql
+++ joins.sql
@@ -1,8 +1,17 @@
 \c has_many_blogs;
 
+-- Create a query to get all fields from the users table
 SELECT *
   FROM users;
 
+-- Create a query to get all fields from the posts table where the user_id is 100
 SELECT *
   FROM posts
   WHERE user_id = '100';
+
+-- Create a query to get all posts fields, the user's first name, and the user's
+-- last name, from the posts table where the user's id is 200
+SELECT posts.*, users.first_name, users.last_name
+  FROM posts
+  INNER JOIN users ON posts.user_id = users.id
+  WHERE posts.user_id = 200;
