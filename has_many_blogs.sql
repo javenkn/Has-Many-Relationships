dc93a5c6e8ac51b3fd29dceebd38fd8cae1f13be
javenkn <javenkn7@yahoo.com>
2016-07-21 19:24:46 -1000

    able to run the provided scripts/blog_data.sql without any errors

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -11,6 +11,8 @@ ALTER DATABASE has_many_blogs OWNER TO has_many_user;
 -- In has_many_blogs.sql Create the tables (including any PKs, Indexes, and Constraints that you may need) to fulfill
 -- the requirements of the has_many_blogs schema below.
 
+\c has_many_blogs;
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -26,17 +28,29 @@ CREATE TABLE posts (
   id SERIAL PRIMARY KEY NOT NULL,
   title character varying(180) NULL DEFAULT NULL,
   url character varying(510) NULL DEFAULT NULL,
-  content text NULL SET DEFAULT NULL,
+  content text NULL DEFAULT NULL,
   created_at timestamp with time zone NOT NULL DEFAULT now(),
-  updated_at timestamp with time zone NOT NULL DEFAULT now()
+  updated_at timestamp with time zone NOT NULL DEFAULT now(),
+  user_id INTEGER REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS comments;
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY NOT NULL,
-  body  character varying(510) NULL DEFAULT NULL,
+  body character varying(510) NULL DEFAULT NULL,
   created_at timestamp with time zone NOT NULL DEFAULT now(),
-  updated_at timestamp with time zone NOT NULL DEFAULT now()
+  updated_at timestamp with time zone NOT NULL DEFAULT now(),
+  user_id INTEGER REFERENCES users(id),
+  post_id INTEGER REFERENCES posts(id)
 );
+
+CREATE INDEX index_for_users
+  ON users (first_name, last_name);
+
+CREATE INDEX index_for_posts
+  ON posts (title, url, content);
+
+CREATE INDEX index_for_comments
+  ON comments (body);
 
 \i scripts/blog_data.sql;
