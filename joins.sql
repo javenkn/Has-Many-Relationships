6a827642c7ce285439370ed37874a2784dc1f515
javenkn <javenkn7@yahoo.com>
2016-07-21 21:01:12 -1000

    finished all tasks for queries across multiple tables

--- joins.sql
+++ joins.sql
@@ -87,3 +87,16 @@ SELECT users.first_name, users.last_name, comments.body AS comment_body
   INNER JOIN posts ON comments.post_id = posts.id
   WHERE comments.body LIKE '%SSL%'
   AND posts.content LIKE '%dolorum%';
+
+-- Create a query to get the first name of the author of the post (aliased to
+-- post_author_first_name), last name of the author of the post (aliased to post_author_last_name),
+-- the post title (aliased to post_title), username of the author of the comment (aliased to
+-- comment_author_username), and comment body (aliased to comment_body), where the comment body
+-- contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo'
+-- ( should have 197 results )
+SELECT users.first_name AS post_author_first_name, users.last_name AS post_author_last_name, posts.title AS post_title, users.username AS comment_author_username, comments.body AS comment_body
+  FROM posts
+  INNER JOIN users ON posts.user_id = users.id
+  INNER JOIN comments ON posts.id = comments.post_id
+  WHERE (lower(comments.body) LIKE '%ssl%' OR lower(comments.body) LIKE '%firewall%')
+  AND (lower(posts.content) LIKE '%nemo%');
